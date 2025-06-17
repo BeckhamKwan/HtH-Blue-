b2748805e33841ddfcb4f5c9602bd39214deb5a7
Beckham Kwan <bk@MacBookPro.attlocal.net>
2025-06-17 14:02:35 -0700

    lab2

---
+++ lab2.py
@@ -0,0 +1,13 @@
+name = "Beckham Kwan"
+
+Nickname = "BK"
+
+Age = 17
+
+used_python = True
+
+Hobbies = ["Gaming" or "VolleyBall"]
+
+Favorite_Things = {'Tech': 'Phone','Games':"F1 25",} 
+{"Genshin Impact" "Honkai Star Rail"}
+
