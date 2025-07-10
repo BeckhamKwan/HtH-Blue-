2aed15b08faab0c84e78d1decb440b4a1292f662
Beckham Kwan <bk@MacBookPro.attlocal.net>
2025-07-10 11:52:48 -0700

    added functions_Activity

---
+++ functions_activity.py
@@ -0,0 +1,13 @@
+#F & OP Beckham,Kwan
+
+menu = {"Pizza": 2.99, "Burger": 3.99, "Hot dog": 1.99, "Cheese": 0.59, "Ice cream": 1.49, "Churro": 0.79, "Soda": 0.89}
+
+def total_price(keys,value):
+    
+  menu = (keys,value)
+
+  return menu
+
+print(menu)
+
+print()
