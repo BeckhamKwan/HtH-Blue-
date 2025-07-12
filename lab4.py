783b59a78bb358f109f8d019f1942683da41a9ab
Beckham Kwan <bk@MacBookPro.attlocal.net>
2025-07-11 17:12:49 -0700

    uncommited lab 4

--- lab4.py
+++ lab4.py
@@ -1,30 +1,34 @@
 #Task 1 
-#checking = "yes"
+checking = "yes"
 
-#while checking == "yes":
-    #print("What is your age")
-    #user_input = input()
+while checking == "yes":
+    print("What is your age")
+    user_input = input()
 
-    #if int(user_input) >= 18:
-        #print("Yes you can vote")
+    if int(user_input) >= 18:
+        print("Yes you can vote")
 
-    #else:
-        #print("You can't vote")
-    #print("Would you like to check another age? " )
-    #user_input2 = input()
-    #checking = user_input2
+    else:
+        print("You can't vote")
+    print("Would you like to check another age? " )
+    user_input2 = input()
+    checking = user_input2
 
 #Task 2 
 
-#list1 = [-3,-1,0,3,6]
+list1 = [-3,-1,0,3,6]
 
-#for x in list:
-  # if x >0:
- #print("The value is positive")
- #elif if x <0:
- # print("The value is Negtiive")
- # else: 
- #  print('0')    
+for x in list:
+   
+   if x > 0:
+    
+     print("The value is positive")
+   
+if x < 0:
+   
+   print("The value is Negtiive")
+else: 
+   print('0')    
 
 
 
@@ -34,17 +38,17 @@
 
 #Task 3 
 
-#Inventory = ["Bedrock","Gold","Iron","CobbleStone","netherite"]
+Inventory = ["Bedrock","Gold","Iron","CobbleStone","netherite"]
 
-#for i in (Inventory):
-   #if i == "Bedrock":
-      #print("Why do you have this in your inventory")
+for i in (Inventory):
+  if i == "Bedrock":
+      print("Why do you have this in your inventory")
 
-   #elif i == "Bedrock":
-    #print("Placed Bedrock arould Johnny Diaz's Home" )
+  elif i == "Bedrock":
+    print("Placed Bedrock arould Johnny Diaz's Home" )
 
-#for i in ("Gold"):
-  #print(f"You got {i} pog")
+for i in ("Gold"):
+  print(f"You got {i} pog")
 
 
 
